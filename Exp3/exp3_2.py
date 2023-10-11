809a264862a4915a92861a03ca44308f06104f04
Void Empty <wxj724881604@gmail.com>
2023-10-11 09:21:10 +0800

    优化入口判断

--- Exp3/exp3_2.py
+++ Exp3/exp3_2.py
@@ -1,6 +1,9 @@
 """用辗转相除法计算两个正整数的最大公约数及最小公倍数"""
 numlist = input("请输入两个正整数，用空格隔开：").split()
 num1, num2 = int(numlist[0]), int(numlist[1])
+if num1 <= 0 or num2 <= 0:
+    print("请按要求输入正整数！")
+    exit(0)
 while num2 != 0:
     num1, num2 = num2, num1 % num2
 print("最大公约数为：%d" % num1)
