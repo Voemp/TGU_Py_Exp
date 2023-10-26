91f0961ded5a32bd5bf70fb618bda86ea4a40177
Void_Empty <wxj724881604@gmail.com>
2023-10-26 16:39:38 +0800

    编程计算100以内的素数（使用函数）

---
+++ Exp4/exp4_2.py
@@ -0,0 +1,15 @@
+"""编程计算100以内的素数（使用函数）"""
+
+
+def is_prime(num):
+    """判断是否为素数"""
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+
+pnlist = [str(num) for num in range(101) if is_prime(num)]
+print(*pnlist, sep=',', end='。\n')
+print("100以内共有{}个素数。".format(len(pnlist)))
