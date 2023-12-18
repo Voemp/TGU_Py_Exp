6cf9683f57a72f3ac030ac7532c684f8b740db08
Void_Empty <wxj724881604@gmail.com>
2023-12-18 20:57:26 +0800

    修正错误

--- Exp3/exp3_3.py
+++ Exp3/exp3_3.py
@@ -1,4 +1,4 @@
 """求20-100之间的素数并输出个数"""
-pnlist = [str(num) for num in range(20, 101) if all(num % i != 0 for i in range(2, num // 2 + 1))]
+pnlist = [str(num) for num in range(20, 101) if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1))]
 print(*pnlist, sep=',')
 print("素数个数为：%s" % len(pnlist))
